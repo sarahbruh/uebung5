711014b807f8b477dcfc354a0e932999e674c36e
Sarah <sarah.brunmayr@htl-wels.at>
2020-10-19 16:32:46 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -34,38 +35,48 @@ public class Main extends Application{
         txt.setText("Grad Celsius");
         txt.setStyle("-fx-font-size:14;-fx-font-weight: bold");
 
+        TextField text = new TextField();
+        text.setMaxWidth(300);
+
         Text txt1 = new Text();
         txt1.setText("Grad Fahrenheit");
         txt1.setStyle("-fx-font-size:14;-fx-font-weight: bold");
 
+        TextField text1 = new TextField();
+        text.setMaxWidth(300);
+
         Text br = new Text("");   //Abstand zwischen Textfeld und Buttons
 
         Button btn1 = new Button();
-        btn1.setText("Convert C -> F");
+        btn1.setText("Convert F -> C");
         btn1.setMinWidth(100);
         btn1.setMinHeight(20);
 
-        EventHandler btn1_handler = new EventHandler() {
+        EventHandler<MouseEvent> btn1_handler = new EventHandler<MouseEvent>() {
             @Override
-            public void handle(Event event) {
+            public void handle(MouseEvent mouseEvent) {
+                /*int fahrenheit = Integer.parseInt(text1.getText());
 
+                int celsius = (fahrenheit * 9/5) + 32;
+
+                text.setText(String.valueOf(celsius));*/
             }
         };
 
         Button btn2 = new Button();
-        btn2.setText("Convert F -> C");
+        btn2.setText("Convert C -> F");
         btn2.setMinWidth(100);
         btn2.setMinHeight(20);
 
-        EventHandler btn2_handler = new EventHandler() {
+        EventHandler<MouseEvent> btn2_handler = new EventHandler<MouseEvent>() {
             @Override
-            public void handle(Event event) {
+            public void handle(MouseEvent mouseEvent) {
 
             }
         };
 
-        vbox.getChildren().addAll( btn1, btn2);
-
+        vbox.getChildren().addAll(txt, text, txt1, text1, br, btn1, btn2);
+        btn1.addEventHandler(MouseEvent.MOUSE_CLICKED, btn1_handler);
         Scene scene = new Scene(vbox);
 
         stage.setScene(scene);
